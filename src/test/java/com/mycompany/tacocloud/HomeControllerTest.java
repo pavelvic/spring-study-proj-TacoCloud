75a9e901c6678d11618619fa2ab34ab50cf91afa
pavelvic <pavelvic@gmail.com>
2020-08-30 00:34:08 +0300

    styles.css and input validation

--- src/test/java/com/mycompany/tacocloud/HomeControllerTest.java
+++ src/test/java/com/mycompany/tacocloud/HomeControllerTest.java
@@ -4,7 +4,6 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,6 +21,5 @@ public class HomeControllerTest {
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andExpect(MockMvcResultMatchers.view().name("home"))
             .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Welcome to...")));
-            //  .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("123")));
     }
 }
